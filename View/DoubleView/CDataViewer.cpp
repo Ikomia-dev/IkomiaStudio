85d917d04b75e0daa38c137dda1328d476b53e42
Ludo <ludovic.barusseau@gmail.com>
2021-02-15 11:59:47 +0100

    Add agplv3 license headers

--- View/DoubleView/CDataViewer.cpp
+++ View/DoubleView/CDataViewer.cpp
@@ -1,4 +1,22 @@
-﻿/**
+// Copyright (C) 2021 Ikomia SAS
+// Contact: https://www.ikomia.com
+//
+// This file is part of the IkomiaStudio software.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
  * @file      CDataViewer.cpp
  * @author    Guillaume Demarcq and Ludovic Barusseau
  * @brief     Implementation file for CDataViewer
