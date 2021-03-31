82531745e631c9525279babc22f0dc13264f53dd
Ludo <ludovic.barusseau@gmail.com>
2021-03-31 08:43:40 +0200

    Update translations

--- Translations/ikomia_fr.ts
+++ Translations/ikomia_fr.ts
@@ -1,24 +1,4 @@
 <?xml version="1.0" encoding="utf-8"?>
-/**
- * Copyright (C) 2021 Ikomia SAS
- * Contact: https://www.ikomia.com
- *
- * This file is part of the IkomiaStudio software.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 <!DOCTYPE TS>
 <TS version="2.1" language="fr_FR">
 <context>
@@ -1646,7 +1626,7 @@ Utiliser les boutons du haut pour charger un projet, des images, des vidéos ou 
     <message>
         <location filename="../View/Notifications/CNotificationPane.cpp" line="149"/>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Protocol</source>
