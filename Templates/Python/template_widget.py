bdb8e80bfe9331753a11424a147a88a7768214dc
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2023-07-18 15:56:19 +0200

    Chore: update Python plugin template

--- Templates/Python/template_widget.py
+++ Templates/Python/template_widget.py
@@ -1,21 +1,3 @@
-# Copyright (C) 2021 Ikomia SAS
-# Contact: https://www.ikomia.com
-#
-# This file is part of the IkomiaStudio software.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from ikomia import core, dataprocess
 from ikomia.utils import pyqtutils, qtconversion
 from _PluginName_._PluginName__process import _PluginClassName_Param
@@ -24,7 +6,7 @@ _QtBindingBlock_
 
 
 # --------------------
-# - Class which implements widget associated with the process
+# - Class which implements widget associated with the algorithm
 # - Inherits PyCore.CWorkflowTaskWidget from Ikomia API
 # --------------------
 class _PluginClassName_Widget(_WidgetBaseClass_):
@@ -45,21 +27,21 @@ class _PluginClassName_Widget(_WidgetBaseClass_):
         # Apply button clicked slot
 
         # Get parameters from widget
-        # Example : self.parameters.windowSize = self.spinWindowSize.value()
+        # Example : self.parameters.window_size = self.spin_window_size.value()
 
-        # Send signal to launch the process
+        # Send signal to launch the algorithm main function
         self.emit_apply(self.parameters)
 
 
 # --------------------
-# - Factory class to build process widget object
+# - Factory class to build algorithm widget object
 # - Inherits PyDataProcess.CWidgetFactory from Ikomia API
 # --------------------
 class _PluginClassName_WidgetFactory(dataprocess.CWidgetFactory):
 
     def __init__(self):
         dataprocess.CWidgetFactory.__init__(self)
-        # Set the name of the process -> it must be the same as the one declared in the process factory class
+        # Set the algorithm name attribute -> it must be the same as the one declared in the algorithm factory class
         self.name = "_PluginName_"
 
     def create(self, param):
