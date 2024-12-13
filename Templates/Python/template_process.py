98254690d18401644942b6fbd522befddaab076e
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2024-12-13 09:29:10 +0100

    chore: update version

--- Templates/Python/template_process.py
+++ Templates/Python/template_process.py
@@ -115,8 +115,8 @@ class _PluginClassName_Factory(dataprocess.CTaskFactory):
         self.info.license = ""
 
         # Ikomia API compatibility
-        # self.info.min_ikomia_version = "0.12.0"
-        # self.info.max_ikomia_version = "0.12.0"
+        # self.info.min_ikomia_version = "0.13.0"
+        # self.info.max_ikomia_version = "0.13.0"
 
         # Python compatibility
         # self.info.min_python_version = "3.10.0"
