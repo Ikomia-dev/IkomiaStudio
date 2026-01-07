c0e341d6a2ebb02895d150d256ef0f23b60d5af2
Ludo <ludovic.barusseau@ikomia.com>
2026-01-07 11:20:38 +0100

    chore: update Windows config (Qt6, msvc 2022)

--- LocalSettingsTemplate.cmake
+++ LocalSettingsTemplate.cmake
@@ -1,17 +1,17 @@
 add_compile_definitions(
-    CUDA12
+    CPU
     PY311
 )
 
-set(OPENCV_GPU ON)
+set(OPENCV_GPU OFF)
 set(PRODUCTION OFF)
 set(PYTHON_VERSION_DOT 3.11)
 set(PYTHON_VERSION_DOT_M 3.11)
 set(PYTHON_VERSION_NO_DOT 311)
 
 if(WIN32)
-    set(BOOST_VERSION 1_83)
-    set(BOOST_VC_VERSION 142)
-    set(MSVC_VERSION 16)
+    set(BOOST_VERSION 1_88)
+    set(BOOST_VC_VERSION 143)
+    set(MSVC_VERSION 17)
     set(CUDA_VERSION 12.1)
 endif()
